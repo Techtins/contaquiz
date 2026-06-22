c2a4c4aeef8fe14321bc128f5987568c7eb1dd75
= <=>
2026-06-22 16:59:14 -0300

    feat: add BaseEntity.java & exception mappers and classes

---
+++ backend/backend-quiz/src/main/java/br/com/techtins/contaquiz/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.techtins.contaquiz.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
